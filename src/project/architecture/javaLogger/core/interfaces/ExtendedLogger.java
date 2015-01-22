638dc2c65364fc56748677bc7a5f0ff883bbd80e
kadary DEMBELE <kadary@demkada.com>
2015-01-22 23:10:56 +0100

    - Finishing AbstractLogger class implementation - Some adjustments on Logger, ExtendeLogger and LevelLogger interfaces -  Adding LogPrinter interface and his implementation in Printer which should be abstract -  Adding AbstractExtendesLogger and AbstractLoggerByLevel

--- src/project/architecture/javaLogger/core/interfaces/ExtendedLogger.java
+++ src/project/architecture/javaLogger/core/interfaces/ExtendedLogger.java
@@ -2,10 +2,16 @@ package project.architecture.javaLogger.core.interfaces;
 
 public interface ExtendedLogger extends LevelLogger {
 	
+	boolean isEnabled(Level level);
 	boolean isEnabled(Level level, String message);
 	boolean isEnabled(Level level, String message, Throwable t);
 	boolean isEnabled(Level level, Tagger tagger, String message);
 	boolean isEnabled(Level level, Tagger tagger, String message, Throwable t);
+	
+	void log(Level level, String message);
+	void log(Level level, String message, Throwable t);
+	void log(Level level, Tagger tagger, String message);
+	void log(Level level, Tagger tagger, String message, Throwable t);
 	
 	void logIfEnabled(String fqcn, Level level, String message);
 	void logIfEnabled(String fqcn, Level level, String message, Throwable t);
--- src/project/architecture/javaLogger/core/interfaces/LevelLogger.java
+++ src/project/architecture/javaLogger/core/interfaces/LevelLogger.java
@@ -5,42 +5,36 @@ public interface LevelLogger extends Logger {
 	boolean isInfoEnabled();
 	void info(String message);
 	void info(String message, Throwable t);
-	boolean isInfoEnabled(Tagger tagger);
 	void info(Tagger tagger, String message);
 	void info(Tagger tagger, String message, Throwable t);
 	
 	boolean isTraceEnabled();
 	void trace(String message);
 	void trace(String message, Throwable t);
-	boolean isTraceEnabled(Tagger tagger);
 	void trace(Tagger tagger, String message);
 	void trace(Tagger tagger, String message, Throwable t);
 	
 	boolean isWarnEnabled();
 	void warn(String message);
 	void warn(String message, Throwable t);
-	boolean isWarnEnabled(Tagger tagger);
 	void warn(Tagger tagger, String message);
 	void warn(Tagger tagger, String message, Throwable t);
 	
 	boolean isDebugEnabled();
 	void debug(String message);
 	void debug(String message, Throwable t);
-	boolean isDebugEnabled(Tagger tagger);
 	void debug(Tagger tagger, String message);
 	void debug(Tagger tagger, String message, Throwable t);
 	
 	boolean isErrorEnabled();
 	void error(String message);
 	void error(String message, Throwable t);
-	boolean isErrorEnabled(Tagger tagger);
 	void error(Tagger tagger, String message);
 	void error(Tagger tagger, String message, Throwable t);
 	
 	boolean isFatalEnabled();
 	void fatal(String message);
 	void fatal(String message, Throwable t);
-	boolean isFatalEnabled(Tagger tagger);
 	void fatal(Tagger tagger, String message);
 	void fatal(Tagger tagger, String message, Throwable t);
 
